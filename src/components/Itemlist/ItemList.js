64216cba5c53426e726107af4345043cbe5c682e
unknown <manuel.rodriguez.ch1@gmail.com>
2022-03-09 18:47:43 -0300

    final

--- src/components/Itemlist/ItemList.js
+++ src/components/Itemlist/ItemList.js
@@ -3,7 +3,7 @@ import { Item } from "../Item/Item";
 
 export const ItemList = ({ Items }) => {
   return (
-    <section className="item-list-container">
+    <section className="row item-list-container">
       {Items.map((el, i) => (
         <Item key={i} {...el} />
       ))}
